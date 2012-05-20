2708ecd978659af6828d6523b7cda5def752274d
KAZUMA Ukyo <yaakaito@gmail.com>
2012-05-21 01:29:47 +0900

    シングルトンのテンプレート追加した

---
+++ Templates/Utility/Objective-C singleton.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
++ (id)___VARIABLE_getterName:identifier___ {
+    
+    static dispatch_once_t pred = 0;
+    __strong static id _sharedObject = nil;
+    dispatch_once(&pred, ^{
+        _sharedObject = [[self alloc] init];
+    });
+    return _sharedObject;    
+}
+@end
