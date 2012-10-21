610ab5e2387be5430066e6d22bcc3e38faa97d03
KAZUMA Ukyo <yaakaito@gmail.com>
2012-10-22 01:34:34 +0900

    add specta template

---
+++ Templates/Specta/Specta.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,17 @@
+//
+//  ___PROJECTNAME___ - ___FILENAME___
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Created by: ___FULLUSERNAME___
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+
+SpecBegin(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
+});
+
+SpecEnd
